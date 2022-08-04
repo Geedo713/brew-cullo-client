eb5cccd49a902bc359027127c678e63e2309b68d
Martin Clark <webdeveloperplug@gmail.com>
2022-08-03 22:16:16 -0500

    some more good old nightly updates fam

--- web-pages/fuctionality.js
+++ web-pages/fuctionality.js
@@ -1,14 +1,18 @@
-function sendEmail() {
-Email.send({
-    Host : "smtp.gmail.com",
-    Username : "language.rugs@gmail.com",
-    Password : "Mujahideen#!187",
-    To : 'webdeveloperplug@gmail.com',
-    From : document.getElementById("name").value,
-    Subject : "Contact Form Test",
-    Body : "Name: " + document.getElementById("name").value 
-    + "<br> Email: " + document.getElementById("email").value
-}).then(
-  message => alert("Message Sent Successfully!")
-);
+//const form = document.getElementById("contact_form");
+
+function redirectCall() {
+    document.getElementById("submit").addEventListener("submit", function(event) {
+      event.preventDefault()
+    });
+    document.getElementById("contact_form").addEventListener("submit", function(e) {
+      if(!isValid) {
+        alert("Something Happened!");
+      } else {
+        window.location.href = "index.html";
+      }
+    });
+    /*document.querySelector('#contact_form')
+  .addEventListener('submit', () => {
+  window.location.href = 'contact-form-thank-you.html';
+  });*/
 }
