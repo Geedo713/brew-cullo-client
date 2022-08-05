52cadd7ddb46d70a593aed00a4789f787623ffc8
Martin Clark <webdeveloperplug@gmail.com>
2022-08-04 19:03:28 -0500

    more updates with a little functionality

--- web-pages/fuctionality.js
+++ web-pages/fuctionality.js
@@ -1,18 +1,8 @@
-//const form = document.getElementById("contact_form");
+const form = document.getElementById("contactForm");
 
 function redirectCall() {
     document.getElementById("submit").addEventListener("submit", function(event) {
       event.preventDefault()
     });
-    document.getElementById("contact_form").addEventListener("submit", function(e) {
-      if(!isValid) {
-        alert("Something Happened!");
-      } else {
-        window.location.href = "index.html";
-      }
-    });
-    /*document.querySelector('#contact_form')
-  .addEventListener('submit', () => {
-  window.location.href = 'contact-form-thank-you.html';
-  });*/
+window.location.href = "contact-form-thank-you.html";
 }
