ed3378971bbf18f4422dd0eb4cf686fb9962273d
Martin Clark <webdeveloperplug@gmail.com>
2022-08-08 23:51:12 -0500

    time to let them things fly fam we gotta get this going ASAP

--- web-pages/fuctionality.js
+++ web-pages/fuctionality.js
@@ -1,8 +1,23 @@
 const form = document.getElementById("contactForm");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const submitBtn = document.getElementById("submit");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzBi86FgtMDh3Zftm-vPRIsOyjeeZdUMstVxNq_k4rg3XQhJLY_C6nTR4U83nhAvEc/exec'
 
-function redirectCall() {
-    document.getElementById("submit").addEventListener("submit", function(event) {
-      event.preventDefault()
-    });
-form.querySelector()
-}
+   form.addEventListener('submit', e => {
+     submitBtn.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitBtn.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitBtn.disabled = false
+    }
+       )
+       form.reset();
+   })
+  console.log("these nuts fam");
