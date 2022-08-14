a3a54c1f7660a36358410a097df1fdc661fa5c02
Martin Clark <webdeveloperplug@gmail.com>
2022-08-14 00:01:22 -0500

    some more nightly updates for the big project build guys lets get it going!!

---
+++ music/script.js
@@ -0,0 +1,56 @@
+let allTracks = {
+    "track1":
+    [
+         "Recession",
+         "music/Recession.mp3",
+         "Cullo"
+    ],
+    "track2":
+    [
+         "APTTMH",
+         "music/APTTMH.mp3",
+         "Cullo"
+    ],
+    "track3": 
+    [
+         "AZ",
+         "music/AZ.mp3",
+         "Cullo"
+    ],
+    "track4":
+    [
+         "Frequency",
+         "music/Frequency.mp3",
+         "Cullo"
+    ],
+    "track5": 
+    [
+         "Indigo",
+         "music/Indigo.mp3",
+         "Cullo"
+    ],
+    "track6": 
+    [
+         "Shabbat Shalom",
+         "music/Shabbat-Shalom.mp3",
+         "Cullo"
+    ],
+    "track7": 
+    [
+        "TD Jakes",
+         "music/TD-Jakes.mp3",
+        "Cullo"
+    ],
+    "track8":
+    [
+        "Shabbat Shalom",
+        "music/Shabbat-Shalom.mp3",
+        "Cullo"
+   ],
+   "track9":
+   [
+    "Shabbat Shalom",
+    "music/Shabbat-Shalom.mp3",
+    "Cullo"
+]
+}
